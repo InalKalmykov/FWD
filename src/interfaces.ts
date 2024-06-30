43d26d2d5006db635b3af3c85eb3799af4a55fc1
InalKalmykov <73423764+InalKalmykov@users.noreply.github.com>
2024-07-01 00:04:24 +0300

    Add files via upload

---
+++ src/interfaces.ts
@@ -0,0 +1,14 @@
+// src/interfaces.ts
+
+export interface Comic {
+  num: number;
+  img: string;
+  alt: string;
+  title: string;
+  day: string;
+  month: string;
+  year: string;
+}
+export interface ComicIdResponse {
+  id: number;
+}
