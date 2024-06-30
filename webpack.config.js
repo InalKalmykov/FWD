d40afc3a93b27668cebf63201ddd47d8ab02c19d
InalKalmykov <73423764+InalKalmykov@users.noreply.github.com>
2024-06-30 22:10:53 +0300

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'production', // или 'development' для отладки
-    entry: './src/index.ts', // Точка входа вашего TypeScript кода
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
+  entry: './src/index.ts', // Replace with your TypeScript entry file
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Path to your HTML template file
+    }),
+  ],
 };
